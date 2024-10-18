58c50ef291ce78628b45805340168f2b4cf89938
Sanjay V <94346737+Sanjay131120@users.noreply.github.com>
2024-10-18 23:21:27 +0530

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -1,31 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int BubbleSort(int arr[], int n){
+void BubbleSort(int arr[], int n) {
     int temp = 0;
-    for (int k =0; k<n-1; k++){
-        for(int i = 0; i<n-k-1; i++){
-        int j = i + 1;
-        if(arr[i] > arr[j]){
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+    for (int k = 0; k < n - 1; k++) {
+        for (int i = 0; i < n - k - 1; i++) {
+            // Compare adjacent elements
+            if (arr[i] > arr[i + 1]) {
+                // Swap if the current element is greater than the next element
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
             }
-        else{
-            continue;
-        }
-        j++;
         }
     }
-    cout << "Sorted Array : " << endl;
-
-    for(int i = 0; i<n; i++){
-        cout<< arr[i] << " ";
+    
+    cout << "Sorted Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
-    }
-int main(){
+    cout << endl; // Added for better formatting
+}
 
-    int arr[6] = {10,1,7,6,14,9};
-    cout<< BubbleSort(arr, 6);
-
-} 
+int main() {
+    int arr[6] = {10, 1, 7, 6, 14, 9};
+    BubbleSort(arr, 6); // Calling the BubbleSort function
+    return 0; // Added return statement for main function
+}
